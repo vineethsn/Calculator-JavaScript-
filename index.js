02e09fffc9b9d3d90dc77cb1cc0f4ae092a556a8
vineethsn <110551060+vineethsn@users.noreply.github.com>
2022-09-05 00:22:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
+buttons.map( button => {
+    button.addEventListener('click', (e) =>{
+    })
+})
